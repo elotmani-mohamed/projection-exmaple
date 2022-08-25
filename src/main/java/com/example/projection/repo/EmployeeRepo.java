a5b2c6c6548ff0425c68d3212a4a7b684a7bb8a4
elotmani-mohamed <90799171+elotmani-mohamed@users.noreply.github.com>
2022-08-25 22:09:41 +0100

    Update EmployeeRepo.java

--- src/main/java/com/example/projection/repo/EmployeeRepo.java
+++ src/main/java/com/example/projection/repo/EmployeeRepo.java
@@ -12,6 +12,9 @@ public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
 
     @Query(value = "select * from employee e where e.name=?1 and e.department=?2",nativeQuery = true)
      EmployeeDTO getEmployeeNameAndDepartement(String name,String department);
+    
+    @Query(value = "select e.name as name,e.department as department  from Employee e where e.name=:name and e.department=:department")
+    EmployeeDTO getEmployeeNameAndDepartementJPQL(String name,String department);
 
     EmployeeDTO findEmployeeByNameAndDepartment(String name, String department);
 
